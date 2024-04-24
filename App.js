4683f7438d2494587d8320d1bab37a9e5a499ce0
Brandon Nguyen <brandon.nguyen0311@gmail.com>
2024-04-24 09:48:49 -0500

    Co-authored-by: owomoist <owomoist@users.noreply.github.com> Co-authored-by: kushal-R-B <kushal-R-B@users.noreply.github.com>

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View, StyleSheet, Image, TextInput, ImageBackground, TouchableHighlight, Alert, Dimensions, ScrollView } from 'react-native';
+import { AppRegistry, Text, View, Image, TextInput, ImageBackground, TouchableHighlight, Alert, ScrollView } from 'react-native';
 import Constants from 'expo-constants';
 import { LinearGradient } from 'react-native';
+import styles from './styles';
 
-let deviceHeight = Dimensions.get('window').height;
-let deviceWidth = Dimensions.get('window').width;
+
 
 
 export default class App extends Component {
@@ -195,7 +195,6 @@ export default class App extends Component {
         </View>
 
 
-
         <View style={{ display: this.state.javaPageDisplay }}>
           <View style={styles.javaPage}>
           <View style={styles.goButt}>
@@ -269,186 +268,5 @@ export default class App extends Component {
         </View>
       </View>
     );
-  }
+  };
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  homeScreen: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    
-  },
-
-  navbar: {
-      flexDirection: 'row',
-      borderWidth: 1,
-      margin: 'auto',
-      width: (deviceWidth/2.25)*2,
-      height: deviceWidth/16,
-  },
-
-  homebutton: {
-    borderWidth: 1,
-  },
-
-  library: {
-    borderWidth: 1,
-  },
-
-  libraryButton: {
-    borderWidth: 1,
-  },
-
-  yourLibrary: {
-    borderWidth: 1,
-  },
-
-  buttonWrapper:{
-   alignItems: 'center',
-   justifyContent: 'center', 
-   borderRadius: 25,
-   overflow: 'hidden',
-
-  },
-
-  addButtonWrapper: {
-      borderWidth: 1,
-      borderRadius: 50,
-      
-
-  },
-
-
-  topText: {
-    height: 100,
-    width: deviceWidth*9,
-    textAlign: 'center',
-    borderWidth: 1,
-    alignItems: 'center',
-    
-  },
-
-  
-
-  bottomText: {
-    borderWidth: 1,
-    width: deviceWidth*.9,
-    height: 65,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginBottom: deviceHeight/8,
-  },
-
-
-  titleLine: {
-    flexDirection: 'row',
-    marginTop: 10,
-    borderWidth: 1,
-    width: deviceWidth*.9,
-    justifyContent:'center',
-    alignText:'center',
-
-  },
-
-  scrollViewContainer:{
-    height:deviceHeight/1.5,
-    
-    
-  },
-  setButtonAnat: {
-    
-    width: (deviceWidth/5)*4,
-    height: (deviceHeight/8)*1.5,
-    marginTop: deviceHeight/20,
-    borderRadius: 25,
-    backgroundColor:'#f0b4f4',
-
-    
-
-  },
-
-  titleBox: {
-    marginLeft: deviceWidth/30,
-    marginTop: deviceHeight/30,
-    
-    width: deviceWidth/2.15,
-    height: deviceHeight/9,
-    borderRadius: 20,
-    backgroundColor: 'rgba(242, 242, 242, .64)',
-    alignItems: 'center',
-    justifyContent: 'center',
-
-
-
-  },
-
-  setButtonGeo: {
-    
-    width: (deviceWidth/5)*4,
-    height: (deviceHeight/8)*1.5,
-    marginTop: deviceHeight/20,
-    borderRadius: 25,
-    backgroundColor:'#68A5FF',
-    
-  },
-
-  setButtonCS: {
-    
-    width: (deviceWidth/5)*4,
-    height: (deviceHeight/8)*1.5,
-    marginTop: deviceHeight/20,
-    borderRadius: 25,
-    backgroundColor:'#93E9F2',
-    
-  },
-
-  eduText: {
-   
-    color: '#BDE0FE',
-    fontSize: 100,
-    fontWeight: 'bold',
-
-  },
-  mateText: {
-    fontSize: 100,
-    fontWeight: 'bold',
-    color:'#D6CCC2'
-  },
-
-  text: {
-    fontWeight: 'bold',
-    fontSize: deviceHeight/25,
-
-  },
-
-  homeText: {
-    fontSize: 30, 
-    margin: 5,
-  },
-  
-  goButt: {
-    alignSelf: 'start',
-    alignItems: 'left',
-    justifyContent: 'left',
-    borderWidth: 1,
-    borderRadius: 25,
-  },
-
-  setAddButton: {
-    
-    width: (deviceWidth/5)*4,
-    height: (deviceHeight/8)*1.5,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: deviceHeight/20,
-    borderRadius: 25,
-    backgroundColor:'#F3D0D7',
-    
-  },
-});
